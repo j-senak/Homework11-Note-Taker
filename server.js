82a93d7cb55433f8ed0b1a49bb06a491fa91f81c
JohnS <senakj2011@hotmail.com>
2020-04-03 22:23:24 -0400

    Cleaned up code

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
-// Dependencies
-// =============================================================
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const { v4: uuidv4 } = require('uuid');
-// Sets up the Express App
-// =============================================================
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 var notes = [];
-// Sets up the Express app to handle data parsing
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Routes
+
 app.use(express.static(__dirname + '/public'));
-// =============================================================
-// Basic route that sends the user first to the AJAX Page
+
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
@@ -31,22 +27,19 @@ app.get("/api/notes", function(req, res) {
     return res.json(r);
   });  
 });
-// Displays a single note, or returns false
+
 app.get("/api/notes/:id", function(req, res) {
   var index = parseInt(req.params.id);
-  console.log(index);
   if (index >= 0 && index < notes.length) {
     res.json(notes[index]);
   } else {
   res.status(404);    
   return res.send("Unable to find note with that ID. Please try again");
   }
-  // return res.json(false);
+
 });
-// Create New Characters - takes in JSON input
 app.post("/api/notes", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
+
   var newNote = req.body;
   newNote.id = uuidv4();
   fs.writeFile("./db/db.json", JSON.stringify(newNote), function(err,r) {
@@ -54,13 +47,6 @@ app.post("/api/notes", function(req, res) {
   })
   res.json(newNote);
 });
-//   // Using a RegEx Pattern to remove spaces from newCharacter
-//   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-//   newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-//   console.log(newCharacter);
-//   characters.push(newCharacter);
-// Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function() {
   console.log(`App listening on http://localhost:${PORT}`);
 });
